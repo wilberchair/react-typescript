4c467136f6a0aeccef003ca38ab0b81a8aa838f6
wilberchair <wilberchair@gmail.com>
2022-05-19 09:10:24 -0300

    feat(react-vite): utilizando expressoes

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,8 +1,34 @@
+
 const App = () => {
+  let name: string = 'wilber';
+  let lastName: string = 'lima'
+  let n1: number = 12;
+  let n2: number = 3;
+  let link: string = 'https://b7web.com.br'
+
+
+  function somar(n1: number, n2: number): number {
+    return n1 + n2;
+  }
+
+  function maiuscula(texto: string) {
+    return texto.charAt(0).toUpperCase() + texto.substr(1);
+  }
+  
   return (
-    <div>
-      Olá, tudo bem?
-    </div>
+    <>
+      <div>
+        Olá {name}, tudo bem?
+      </div>
+      <div>
+        Multiplicação: {n1 * n2}
+        Soma: {somar(n1, n2)}
+      </div>
+      <div>
+        {`${maiuscula(name)} ${maiuscula(lastName)}`}
+      </div>
+      <a href={link}>Clique aqui para acessar</a>
+    </>
   )
 }
 
