a91ff5098a11ac0d6ffa9c2733480c9ff8dadfe8
wilberchair <wilberchair@gmail.com>
2022-05-22 22:26:43 -0300

    feat(react-vite): contador usando state

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -2,27 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   let [numero, setNumero] = useState(0)
-  let [nome, setNome] = useState('Wilber')
-  //Se eu criar uma funcao para usar no evento, eu so passso a referencia
-  //Agora se eu nao quiser, é só criar uma funcao anonima e executar ela direto no onClick
   
-  const handleClick = () => {
-    // return alert('Cliquei');
-    setNome('Wilber2')
+  const handleMore = () => {
+    setNumero(numero + 1);
   }
 
-  const handleClickNumber = () => {
-    // return alert('Cliquei');
-    setNumero(numero++)
+  const handleMinus = () => {
+    setNumero(numero - 1)
   }
 
   return (
     <div>
-      O meu nome é {nome}
-      O meu numero é {numero}
-      {/* caso nao tenha unma funcao para passar a referencia no evento, crio uma funcao para executar */}
-      {/* <button onClick={() => alert('Oi')}>Clique aqui</button> */}
-      <button onClick={handleClickNumber}>Clique aqui</button>
+      <button onClick={handleMore}>+</button>
+      <div>{numero}</div>
+      <button onClick={handleMinus}>-</button>
     </div>
   )
 }
