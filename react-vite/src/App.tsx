4ae6658544f4655bc8fff62ff5eff7acf9c6a6a2
wilberchair <wilberchair@gmail.com>
2022-06-12 20:13:14 -0300

    chore(react): states e eventos

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 
 const App = () => {
+  // const [color, setColor] = useState('#FF0000')
+  const [padding, setPadding] = useState(0)
+
+  // const handleClick = () => {
+  //   setColor('#00FF00')
+  // }
+  const handleClick = () => {
+    setPadding(20)
+  }
   return (
     <div>
-      <button style={{
+      <button onClick={handleClick} style={{
         backgroundColor: '#FF0000',
         color: '#FFF',
-        border: 0
+        border: 0,
+        padding
       }}>Clique Aqui</button>
     </div>
   )
