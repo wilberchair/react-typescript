a48380286c264001fc06aa774cc3adfe077350f3
wilberchair <wilberchair@gmail.com>
2022-06-18 17:15:25 -0300

    chore(react): ajustes no tailwind

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,14 +1,6 @@
-// import { useState } from 'react'
-import * as C from './Appstyles';
-
 const App = () => {
   return (
-    <C.Container bgColor={'#BBC'}>
-      <span>Texto do componente</span>
-      <a href='https://google.com' className='link'>Clique aqui</a>
-      <C.Button bg={'#FF0'} small>Clique aqui</C.Button>
-      <C.Button bg={'#F52'}>Clique aqui</C.Button>
-    </C.Container>
+    <div className='bg-black'>Olá</div>
   )
 }
 
