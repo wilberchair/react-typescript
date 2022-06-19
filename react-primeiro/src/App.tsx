945718f58e9e1950b6fb6a3b9f7a100d474ef916
wilberchair <wilberchair@gmail.com>
2022-06-19 18:40:26 -0300

    feat(react): utilizando classes no tailwind

--- react-primeiro/src/App.tsx
+++ react-primeiro/src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-blue-500 text-white p-3 text-xl font-bold">
+      Oi
+      <button className='bg-green-300 p-3 border-3 rounded-sm'>Clique aqui</button>
     </div>
   );
 }
