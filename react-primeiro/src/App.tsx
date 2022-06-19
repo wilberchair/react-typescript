704a4f9b95af8d7570dfc2379a9973c0c554394c
wilberchair <wilberchair@gmail.com>
2022-06-19 19:13:30 -0300

    chore(react): resonsiviadde

--- react-primeiro/src/App.tsx
+++ react-primeiro/src/App.tsx
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App bg-blue-500 text-white p-3 text-xl font-bold">
-      Oi
-      <button className='bg-green-300 p-3 border-3 rounded-sm'>Clique aqui</button>
+    <div className="App bg-blue-500 text-white p-3 text-xl font-bold flex flex-col md:flex-row md:justify-between">
+      Teste criando elemento
+      <button className='bg-green-300 p-3 border-3 rounded mt-3 md:ml-3 md:mt-0'>Clique aqui</button>
     </div>
   );
 }
