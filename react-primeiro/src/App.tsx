dbd2ccfda96ba539dd12e6885815de794162e669
wilberchair <wilberchair@gmail.com>
2022-06-19 20:04:10 -0300

    style(react): estilizndo os estados

--- react-primeiro/src/App.tsx
+++ react-primeiro/src/App.tsx
@@ -2,9 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App bg-blue-500 text-white p-3 text-xl font-bold flex flex-col md:flex-row md:justify-between">
-      Teste criando elemento
-      <button className='bg-green-300 p-3 border-3 rounded mt-3 md:ml-3 md:mt-0'>Clique aqui</button>
+    <div className="App bg-blue-300 p-5">
+      <input
+        className='outline-none border border-transparent bg-white p-3 rounded focus:ring-2 focus:ring-blue-700'
+        type="text"
+        placeholder='Digite alguma coisa'
+      />
+      <button className='ml-2 p-3 bg-blue-400 rounded text-white font-bold hover:bg-blue-500'>Clique aqui</button>
     </div>
   );
 }
