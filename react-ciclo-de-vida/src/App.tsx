1c8ac553de994a3c188addf44916c2f256456ba3
wilberchair <wilberchair@gmail.com>
2022-08-01 16:32:37 -0300

    chore(react): incluindo loading na pagina antes de carregar o conteudo

--- react-ciclo-de-vida/src/App.tsx
+++ react-ciclo-de-vida/src/App.tsx
@@ -4,11 +4,12 @@ import { Movie } from './types/Movie'
 
 const App = () => {
   const [movies, setMovies] = useState<Movie[]>([])
+  const [loading, setLoading] = useState(false)
  
   //caso eu queira carregar os filmes logo que a tela carregar!
-  // useEffect(()=>{
-  //   loadMovies()
-  // }, [])
+  useEffect(()=>{
+    loadMovies()
+  }, [])
 
   //fetch tradicional.. e embaixo da forma mais simples com async await
   // const loadMovies = () => {
@@ -21,24 +22,33 @@ const App = () => {
   // }
 
   const loadMovies = async () => {
+    setLoading(true)
     const response = await fetch('https://api.b7web.com.br/cinema/')
     const json = await response.json()  
-    
+    setLoading(false)
     setMovies(json)
   }
 
   return (
     <div className="App">
-      <button className='button-movie' onClick={loadMovies}>Carregar Filmes...</button>
-      Total de Filmes: {movies.length}
-      <div className='movies-block'>
-        {movies.map((movie, index)=>(
-          <div className='movies'>
-            <p key={index} className='movies-title'>{movie.titulo}</p>
-            <img className='movies-image' src={movie.avatar} alt={movie.titulo} />
+      {/* <button className='button-movie' onClick={loadMovies}>Carregar Filmes...</button> */}
+      {!loading &&
+        <>
+          <div>Total de Filmes: {movies.length}</div>
+          <div className='movies-block'>
+            {movies.map((movie, index)=>(
+              <div key={index} className='movies'>
+                <p className='movies-title'>{movie.titulo}</p>
+                <img className='movies-image' src={movie.avatar} alt={movie.titulo} />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      }
+
+      {loading &&
+        <div>Carregando...</div>
+      }
     </div>
   );
 }
