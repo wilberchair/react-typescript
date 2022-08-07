0062c9f6954a7209ca5ab9966e59501ad9697aa3
wilberchair <wilberchair@gmail.com>
2022-08-06 23:24:35 -0300

    chore(react): utilizando o metodo create do axios

--- react-ciclo-de-vida/src/api.ts
+++ react-ciclo-de-vida/src/api.ts
@@ -1,14 +1,19 @@
 import axios from 'axios'
 
-const BASE = 'https://jsonplaceholder.typicode.com'
+// const BASE = 'https://jsonplaceholder.typicode.com'
+
+const axiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+})
 
 export const api = {
   getAllPosts: async () => {
-    const response = await axios.get(`${BASE}/posts`)
+    // const response = await axios.get(`${BASE}/posts`)
+    const response = await axiosInstance.get('/posts')
     return response.data;
   },
   addNewPost: async (title: string, body: string, userId: number) => {
-    let response = await axios.post(`${BASE}/posts`, {
+    let response = await axiosInstance.post('/posts', {
       title, body, userId
     })
     return response.data;
