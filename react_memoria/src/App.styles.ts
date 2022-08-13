a783d23d37ff2ec505182a00a62d906dd233ee61
wilberchair <wilberchair@gmail.com>
2022-08-12 21:13:38 -0300

    chore(react): criando componentes e utilizando styled

---
+++ react_memoria/src/App.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: block
+`
+
+export const Info = styled.div`` 
+
+export const LogoLink = styled.a`` 
+
+export const InfoArea = styled.div``
+
+export const GridArea = styled.div``
--- react_memoria/src/App.tsx
+++ react_memoria/src/App.tsx
@@ -1,6 +1,21 @@
+import * as C from './App.styles'
+import logoImage from './assets/devmemory_logo.png'
 const App = () => {
   return (
-    <div>OI.....</div>
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width="200" alt="" />
+        </C.LogoLink>
+        <C.InfoArea>
+          ...
+        </C.InfoArea>
+        <button>Reiniciar</button>
+      </C.Info>
+      <C.GridArea>
+
+      </C.GridArea>
+    </C.Container>
   )
 }
 
