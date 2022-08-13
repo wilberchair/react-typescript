207a2f2a2af42a1db3619a155d104e4363a91fd7
wilberchair <wilberchair@gmail.com>
2022-08-13 11:04:14 -0300

    feat(react): organizando pastas e criando componente InfoItem e sua estilizacao

--- react_memoria/src/App.styles.ts
+++ react_memoria/src/App.styles.ts
@@ -1,13 +1,50 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  display: block
+  width: 100%;
+  max-width: 750px;
+  margin: auto;
+  display: flex;
+  padding: 5px 0;
+
+  @media (max-width: 750px) {
+    flex-direction: column;
+  }
 `
 
-export const Info = styled.div`` 
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: auto;
 
-export const LogoLink = styled.a`` 
+  @media (max-width: 750px) {
+    margin-bottom: 50px;
+    align-items: center;
+  }
+` 
 
-export const InfoArea = styled.div``
+export const LogoLink = styled.a`
+  display: block;
+` 
 
-export const GridArea = styled.div``
+export const InfoArea = styled.div`
+  width: 100%;
+  margin: 10px 0;
+
+  @media (max-width: 750px) {
+    display: flex;
+    justify-content: space-around;
+    text-align: center;
+  }
+`
+
+export const GridArea = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+
+  @media (max-width: 750px) {
+    justify-content: center;
+    margin: 0 20px;
+  }
+`
