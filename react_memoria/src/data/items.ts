5ccd9bb0c7f34bf1fb3647c4de23a262c0de8374
wilberchair <wilberchair@gmail.com>
2022-08-13 12:16:05 -0300

    feat(react): fazendo os imports dos svgs

---
+++ react_memoria/src/data/items.ts
@@ -0,0 +1,17 @@
+import androidSvg from '../svgs/android.svg'
+import b7 from '../svgs/b7.svg'
+import caminhao from '../svgs/caminhao.svg'
+import disney from '../svgs/disney.svg'
+import estrela from '../svgs/estrela.svg'
+import gasolina from '../svgs/gasolina.svg'
+import moto from '../svgs/moto.svg'
+
+export const items = [
+  { name: 'android', icon: androidSvg }
+  { name: 'b7', icon: b7 }
+  { name: 'caminhao', icon: caminhao }
+  { name: 'disney', icon: disney }
+  { name: 'estrela', icon: estrela }
+  { name: 'gasolina', icon: gasolina }
+  { name: 'moto', icon: moto }
+]
