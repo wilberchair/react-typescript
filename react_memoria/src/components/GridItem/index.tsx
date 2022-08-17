cc7f1de9c4d77e5ff4e04d0897e184fe2ef1ddec
wilberchair <wilberchair@gmail.com>
2022-08-16 22:57:37 -0300

    chore(react): incluindo os icones do jogo e a condicao se houver ou nao imagem

--- react_memoria/src/components/GridItem/index.tsx
+++ react_memoria/src/components/GridItem/index.tsx
@@ -1,6 +1,7 @@
 import { GridItemType } from '../../types/GridTypes'
 import * as C from './styles'
-
+import b7Svg from '../../svgs/b7.svg'
+import { items } from '../../data/items'
 type Props = {
   item: GridItemType,
   onClick: () => void
@@ -9,7 +10,12 @@ type Props = {
 export const GridItem = ({item, onClick}: Props) => {
   return (
     <C.Container onClick={onClick}>
-      ...
+    {!item.permanentShow && !item.show &&
+      <C.Icon src={b7Svg} alt="" />
+    }
+    {(item.permanentShow || item.show) && item.item !== null &&
+      <C.Icon src={items[item.item].icon} alt="" />
+    }
     </C.Container>
   )
 }
