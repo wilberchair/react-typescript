a28ddb579762d8568316976cd266632722fcf0a4
wilberchair <wilberchair@gmail.com>
2023-05-04 22:25:53 -0300

    arrumando projeto para novos testes

--- react-primeiro/src/App.tsx
+++ react-primeiro/src/App.tsx
@@ -1,16 +1,14 @@
+// import {useEffect, useState} from 'react'
+import { CryptoList } from './components/CryptoList'
+// import axios from 'axios'
+
 import './App.css';
 
-function App() {
+export function App() {
   return (
     <div className="App bg-blue-300 p-5">
-      <input
-        className='outline-none border border-transparent bg-white p-3 rounded focus:ring-2 focus:ring-blue-700'
-        type="text"
-        placeholder='Digite alguma coisa'
-      />
-      <button className='ml-2 p-3 bg-blue-400 rounded text-white font-bold hover:bg-blue-500'>Clique aqui</button>
+      <h1>Crypto Gallerydssdsss</h1>
+      <CryptoList />
     </div>
   );
 }
-
-export default App;
---
+++ react-primeiro/src/components/CryptoList/index.jsx
@@ -0,0 +1,5 @@
+export function CryptoList() {
+  return (
+    <div>CriptoList</div>
+  );
+}
